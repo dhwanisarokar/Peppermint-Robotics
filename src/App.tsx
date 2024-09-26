f31e4638961a48991ddaf93a4091e8f98d2ed174
Nimesh2106 <nimeshsarokar2006@gmail.com>
2024-09-27 00:29:00 +0530

    some UI changes

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import Header from "./components/header";
+import Passcode from "./components/passcode";
+
+function App() {
+  useEffect(() => {
+    document.title = "Peppermint OS";
+  }, []);
+
+  return (
+    <div className="bg-[#222626] text-white h-screen flex flex-col">
+      <Header />
+      <main className="flex flex-1 justify-between p-8">
+        <div className="w-1/2 flex justify-center">
+          <Passcode />
+        </div>
+        <div className="w-1/2 flex justify-center items-center">
+          <img
+            src="/src/assets/image.png"
+            alt="Peppermint Robot"
+            className="w-[620px] h-[500px]" 
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
